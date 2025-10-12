c85dddaa9d7f99418e1e3f70ec65f0dbef7e4be3
Jhordan Roly <jhordanroly22@gmail.com>
2025-10-12 11:49:19 -0500

    Subiendo proyecto completo

---
+++ application/models/Usuario_model.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Usuario_model extends CI_Model {
+
+    public function verificar_usuario($correo, $contraseña) {
+        $this->db->where('correo', $correo);
+        $this->db->where('contraseña', $contraseña); // 🔐 Mejora esto con password_hash en producción
+        $query = $this->db->get('usuarios');
+
+        return $query->row(); // Devuelve objeto si encuentra
+    }
+
+    public function insertar($data) {
+        return $this->db->insert('usuarios', $data);
+    }
+    public function obtener_por_id($id_usuario) {
+    $this->db->where('id_usuario', $id_usuario);
+    $query = $this->db->get('usuarios');
+    if ($query->num_rows() > 0) {
+        return $query->row();
+    }
+    return false;
+}
+
+public function obtener_clientes() {
+    $this->db->select('id_usuario, nombre');
+    $this->db->from('usuarios');
+    $this->db->where('rol', 'cliente');
+    $this->db->order_by('nombre', 'ASC');
+    return $this->db->get()->result();
+}
+
+
+
+
+
+}
